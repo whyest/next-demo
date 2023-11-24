aeb21c5ee344c9969babb5498fdca2bf36c0e547
whyest <whyest@gmail.com>
2023-11-24 16:21:13 +0300

    add loading and fetch logic

---
+++ app/drinks/loading.js
@@ -0,0 +1,4 @@
+const loading = () => {
+  return <span className="loading mx-auto mt-14 block"></span>;
+};
+export default loading;
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,22 @@
-const DrinksPage = () => {
-  return <div>DrinksPage</div>
-}
-export default DrinksPage
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+const fetchDrinks = async () => {
+  await new Response((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(url);
+  //throw error
+  if (!response.ok) {
+    throw new Error("Failed to fetch drinks");
+  }
+  const data = await response.json();
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
+  return (
+    <div>
+      <h1 className="text-7xl">DrinksPage</h1>
+    </div>
+  );
+};
+export default DrinksPage;
