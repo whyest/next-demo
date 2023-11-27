b5a9e77e57dd54639e68255c4bd79b90615ad09c
whyest <whyest@gmail.com>
2023-11-27 17:41:16 +0300

    modify DrinksList

--- app/components/DrinksList.jsx
+++ app/components/DrinksList.jsx
@@ -1,4 +1,21 @@
-const DrinksList = () => {
-  return <div>DrinksList</div>;
+import Link from "next/link";
+
+const DrinksList = ({ drinks }) => {
+  return (
+    <ul className="menu menu-vertical pl-0">
+      {drinks.map((drink) => {
+        return (
+          <li key={drink.idDrink}>
+            <Link
+              href={`/drinks/${drink.idDrink}`}
+              className="text-xl font-medium"
+            >
+              {drink.strDrink}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 export default DrinksList;
